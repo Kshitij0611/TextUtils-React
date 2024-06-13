3ac0c4aed81a8e035a1b0e0dcf43539c01aa1276
KshitijKrCh <2021cs_kshitijkumarchandrakar_a@nie.ac.in>
2024-06-13 20:04:31 +0530

    Second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
-          {props.title}
-        </Link>
+        {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button
           className="navbar-toggler"
           type="button"
@@ -23,15 +22,13 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
+              {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                {props.aboutText}
-              </Link>
-            </li>
+            {/* <li className="nav-item">
+              <Link className="nav-link" to="/about">{props.aboutText}</Link>
+              <a className="nav-link" href="/about">{props.aboutText}</a>
+            </li> */}
           </ul>
           {/* <form className="d-flex">
             <input
@@ -56,10 +53,10 @@ export default function Navbar(props) {
   );
 }
 
-// Navbar.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   aboutText: PropTypes.string,
-// };
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  aboutText: PropTypes.string,
+};
 
 // Navbar.defaultProps = {
 //     title: "Set title here",
